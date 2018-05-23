75d985012331963d9563f8b449c56447328947ad
Felipe Joglar <felipejoglar@gmail.com>
2018-05-23 19:03:06 +0200

    feat: Wire up project dependencies
    Wire up project dependencies so that the button can now pass
    the joke from the Java Library to the Android Library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,12 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
+import com.fjoglar.jokescreen.JokeActivity;
 import com.fjoglar.thejoker.TheJoker;
 
 
@@ -42,7 +43,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, TheJoker.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent jokeIntent = new Intent(this, JokeActivity.class);
+        jokeIntent.putExtra(JokeActivity.EXTRA_JOKE, TheJoker.getJoke());
+
+        startActivity(jokeIntent);
     }
 
 
