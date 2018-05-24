0a454974d1a8cc6ed9f2923e2255132f1cabb44c
Felipe Joglar <felipejoglar@gmail.com>
2018-05-24 19:24:44 +0200

    feat: Create an AsyncTask to retrieve jokes

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/GetJokeAsyncTask.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 Felipe Joglar Santos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.udacity.gradle.builditbigger;
+
+import android.os.AsyncTask;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+
+public class GetJokeAsyncTask extends AsyncTask<com.udacity.gradle.builditbigger.MainActivityFragment, Void, String> {
+
+    private static MyApi mMyApiService = null;
+    private com.udacity.gradle.builditbigger.MainActivityFragment mMainActivityFragment;
+
+
+    @Override
+    protected String doInBackground(com.udacity.gradle.builditbigger.MainActivityFragment... params) {
+
+        mMainActivityFragment = params[0];
+
+        if(mMyApiService == null) {
+            MyApi.Builder builder = new
+                    MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
+
+                                throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+
+            mMyApiService = builder.build();
+        }
+
+        try {
+            return mMyApiService.getJoke().execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        mMainActivityFragment.startJokeScreen(result);
+    }
+}
