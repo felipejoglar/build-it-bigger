2d05a36c84b3176d05dbaa209fb73c3f8d9ad4d7
Felipe Joglar <felipejoglar@gmail.com>
2018-05-24 18:24:24 +0200

    feat: Make GCE pull jokes from the Java Library

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,10 +1,9 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.fjoglar.thejoker.TheJoker;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-
-import javax.inject.Named;
 
 /** An endpoint class we are exposing */
 @Api(
@@ -18,12 +17,10 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke(){
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
+        response.setData(TheJoker.getJoke());
         return response;
     }
 
