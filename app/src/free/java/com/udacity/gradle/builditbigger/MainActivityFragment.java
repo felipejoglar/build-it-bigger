86a9270a585f64039b1670463e9ffc5d3561b551
Felipe Joglar <felipejoglar@gmail.com>
2018-05-26 13:17:51 +0200

    feat: Add intersticial ad

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.VisibleForTesting;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.fjoglar.jokescreen.JokeActivity;
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -24,6 +28,10 @@ public class MainActivityFragment extends Fragment {
 
     private boolean mIsTesting = false;
     private String mJoke;
+    private InterstitialAd mInterstitialAd;
+
+    @BindView(R.id.adView)
+    AdView mAdView;
 
     public MainActivityFragment() {
     }
@@ -37,15 +45,29 @@ public class MainActivityFragment extends Fragment {
 
         ButterKnife.bind(this, root);
 
-        AdView mAdView = root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId(getString(R.string.intersticial_ad_unit_id));
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                if (!mIsTesting) {
+                    startJokeScreen();
+                }
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
+
+        mInterstitialAd.loadAd(adRequest);
         mAdView.loadAd(adRequest);
-        return root;
     }
 
     @OnClick(R.id.button_main_get_joke)
@@ -55,8 +77,10 @@ public class MainActivityFragment extends Fragment {
 
     public void onJokeRetrieved(String joke) {
         mJoke = joke;
-        if (!mIsTesting) {
-            startJokeScreen();
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAG", "The interstitial wasn't loaded yet.");
         }
     }
 
